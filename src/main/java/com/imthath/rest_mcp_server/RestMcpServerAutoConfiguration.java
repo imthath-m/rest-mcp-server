3fbe5ecd0e22fd3ead558677aa164930fe132be3
Mohammed Imthathullah <imthath.m@hotmail.com>
2025-04-17 06:52:36 +0530

    change bean type to common interface

--- src/main/java/com/imthath/rest_mcp_server/RestMcpServerAutoConfiguration.java
+++ src/main/java/com/imthath/rest_mcp_server/RestMcpServerAutoConfiguration.java
@@ -1,5 +1,6 @@
 package com.imthath.rest_mcp_server;
 
+import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
@@ -9,7 +10,7 @@ public class RestMcpServerAutoConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
-    public RestToolCallbackProvider restToolCallbackProvider() {
+    public ToolCallbackProvider restToolCallbackProvider() {
         return new RestToolCallbackProvider();
     }
 } 
